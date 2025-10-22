16bb9af8bd7a5ac0f343f42794fefaad6be5216a
Abraham Gracco <159852551+abrahamgracco@users.noreply.github.com>
2025-10-22 06:59:30 +0000

    replace wrong word

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 function Home() {
   return (
     <h1>
-      Bom dia, minha Rainha. Só para mostrar que continuo estudanto tecnologia!
+      Bom dia, minha Rainha. Só para mostrar que continuo estudando tecnologia.
+      A direção é mais importante do que a velocidade!
     </h1>
   );
 }
