82a3b9ed1a2016f34eb2c120d3d3d2a6e0238ede
Abraham Gracco <159852551+abrahamgracco@users.noreply.github.com>
2025-10-22 07:02:36 +0000

    another version

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <h1>
       Bom dia, minha Rainha. Só para mostrar que continuo estudando tecnologia.
-      A direção é mais importante do que a velocidade!
+      A direção é mais importante do que a velocidade! Você concorda?
     </h1>
   );
 }
